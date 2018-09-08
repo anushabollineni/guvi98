1b80eee7d764227a73d69c7e0f68fa881a65527c
anushabollineni <41730220+anushabollineni@users.noreply.github.com>
2018-09-08 09:54:51 +0530

    Create guvi98.java

---
+++ guvi98.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+   int n=sc.nextInt();
+   int a[]=new int[n];
+for(int i=0;i<n;i++)
+{
+a[i]=sc.nextInt();
+}
+int p=0,c=0;
+for(int i=0;i<n;i++)
+{
+for(int j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+p=i;
+c=1;
+}
+}
+}
+if(c==0)
+System.out.println(n/2);
+else
+System.out.println(p+1);
+	}
+}
